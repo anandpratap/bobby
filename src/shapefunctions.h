3188415a06dd2e5f83c0f60d01ab0cc8a1760f9b
Anand Pratap Singh <apsingh@altair.com>
2016-08-03 14:39:15 -0700

    Changed format of 1D shape function to adhere to the new multidimensional format

--- src/shapefunctions.h
+++ src/shapefunctions.h
@@ -3,38 +3,47 @@
 
 #include "common.h"
 
+
 class ShapeFunction1D{
  private:
-	double __chi_min;
-	double __chi_max;
-	double (*__function_array[2]) (double chi);
-	double (*__dfunction_array[2]) (double chi);
-	
+	double *__chi_min;
+	double *__chi_max;
  public:
 	ShapeFunction1D(){
-		
-		auto f_0 = [](double chi){return 0.5*(1.0-chi);};
-		__function_array[0] = f_0;
-		auto f_1 = [](double chi){return 0.5*(1.0+chi);};
-		__function_array[1] = f_1;
-
-		auto df_0 = [](double chi){return -0.5;};
-		__dfunction_array[0] = df_0;
-		auto df_1 = [](double chi){return 0.5;};
-		__dfunction_array[1] = df_1;
-
+		__chi_min = new double[1]();
+		__chi_max = new double[1]();
+		__chi_min[0] = -1.0;
+		__chi_max[0] = 1.0;
 	}
 
-	double value(double chi, unsigned int mode = 0){
-		assert(mode == 0 || mode == 1);
-		return (*__function_array[mode])(chi);
+	~ShapeFunction1D(){
+		delete[] __chi_min; delete[] __chi_max;
+	}
+
+	double value(double *chi, unsigned int node = 0){
+		assert(node == 0 || node == 1);
+		
+		if(node == 0){
+			return 0.5*(1.0 - chi[0]);
+		}
+		else if(node == 1){
+			return 0.5*(1.0 + chi[0]);
+		}
 	};
-	double derivative(double chi, unsigned int mode = 0){
-		assert(mode == 0 || mode == 1);
-		return (*__dfunction_array[mode])(chi);
+	double derivative(double *chi, unsigned int node = 0, unsigned int direction = 0){
+		assert(node == 0 || node == 1);
+		assert(direction == 0);
+		
+		if(direction == 0){
+			if(node == 0){
+				return -0.5;
+			}
+			else if(node == 1){
+				return 0.5;
+			}
+		}
 	};
 };
-
 
 class ShapeFunction2D{
  private:
@@ -56,53 +65,53 @@ class ShapeFunction2D{
 		delete[] __chi_min; delete[] __chi_max;
 	}
 
-	double value(double *chi, unsigned int mode = 0){
-		assert(mode == 0 || mode == 1 || mode == 2 || mode == 3);
+	double value(double *chi, unsigned int node = 0){
+		assert(node == 0 || node == 1 || node == 2 || node == 3);
 		
-		if(mode == 0){
+		if(node == 0){
 			return 0.25*(1.0 - chi[0])*(1.0 - chi[1]);
 		}
-		else if(mode == 1){
+		else if(node == 1){
 			return 0.25*(1.0 + chi[0])*(1.0 - chi[1]);
 		}
-		else if(mode == 2){
+		else if(node == 2){
 			return 0.25*(1.0 + chi[0])*(1.0 + chi[1]);
 		}
-		else if(mode == 3){
+		else if(node == 3){
 			return 0.25*(1.0 - chi[0])*(1.0 + chi[1]);
 		}
 		
 	};
-	double derivative(double *chi, unsigned int mode = 0, unsigned int direction = 0){
-		assert(mode == 0 || mode == 1 || mode == 2 || mode == 3);
+	double derivative(double *chi, unsigned int node = 0, unsigned int direction = 0){
+		assert(node == 0 || node == 1 || node == 2 || node == 3);
 		assert(direction == 0 || direction == 1);
 
 		if(direction == 0){
-			if(mode == 0){
+			if(node == 0){
 				return -0.25*(1.0 - chi[1]);
 			}
-			else if(mode == 1){
+			else if(node == 1){
 				return 0.25*(1.0 - chi[1]);
 			}
-			else if(mode == 2){
+			else if(node == 2){
 				return 0.25*(1.0 + chi[1]);
 			}
-			else if(mode == 3){
+			else if(node == 3){
 				return -0.25*(1.0 + chi[1]);
 			}
 	
 		}
 		else if (direction == 1){
-			if(mode == 0){
+			if(node == 0){
 				return -0.25*(1.0 - chi[0]);
 			}
-			else if(mode == 1){
+			else if(node == 1){
 				return -0.25*(1.0 + chi[0]);
 			}
-			else if(mode == 2){
+			else if(node == 2){
 				return 0.25*(1.0 + chi[0]);
 			}
-			else if(mode == 3){
+			else if(node == 3){
 				return 0.25*(1.0 - chi[0]);
 			}
 		}
